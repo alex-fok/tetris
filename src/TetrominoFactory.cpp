67d76000b5dd8be15127c98a67b1a2e4be91a23c
Alex Fok <foktmalexander@gmail.com>
2023-10-15 06:08:50 -0700

    Use random tetromino

--- src/TetrominoFactory.cpp
+++ src/TetrominoFactory.cpp
@@ -10,7 +10,7 @@ GameEntity::TetrominoFactory::TetrominoFactory() :
 
 GameEntity::Tetromino * GameEntity::TetrominoFactory::generateTetromino()
 {
-    // GameEntity::BlockType type = GameEntity::BlockType(rand() % 7 + 1);
-    GameEntity::BlockType type = GameEntity::BlockType(I);
+    GameEntity::BlockType type = GameEntity::BlockType(rand() % 7 + 1);
+    // GameEntity::BlockType type = GameEntity::BlockType(I);
     return new Tetromino(m_currIdx++, type);
 }
