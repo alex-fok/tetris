2c207c89883f111fa4491020bda62633d5a1bf40
Alex Fok <foktmalexander@gmail.com>
2023-11-17 11:32:39 -0800

    Rename member variables

--- src/GameUI/UI.cpp
+++ src/GameUI/UI.cpp
@@ -4,8 +4,8 @@
 
 GameUI::UI::UI():
     m_window(sf::VideoMode((unsigned int)Config::Window::Width, (unsigned int)Config::Window::Height), "Tetris"),
-    m_container(&m_window, Config::Block::Size, Config::TetrominoContainer::BorderWidth, Config::TetrominoContainer::Window_Offset),
-    m_gameOver_menu(Menu::GameOver(&m_window))
+    m_tetrominoContainer(&m_window, Config::Block::Size, Config::TetrominoContainer::BorderWidth, Config::TetrominoContainer::Window_Offset),
+    m_gameOverMenu(Menu::GameOver(&m_window))
 {
 }
 
@@ -23,8 +23,8 @@ void GameUI::UI::run()
     int iteration = 0;
     while (m_window.isOpen())
     {
-        if (!isPaused && !m_container.isGameOver)
-            m_container.nextStep();
+        if (!isPaused && !m_tetrominoContainer.isGameOver)
+            m_tetrominoContainer.nextStep();
 
         sf::Event event;
         
@@ -35,7 +35,7 @@ void GameUI::UI::run()
                     m_window.close();
                     break;
                 case sf::Event::KeyPressed:
-                    if (m_container.isGameOver)
+                    if (m_tetrominoContainer.isGameOver)
                         break;
                     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
                         isPaused = !isPaused;
@@ -43,7 +43,7 @@ void GameUI::UI::run()
                         for (sf::Keyboard::Key k: inputs)
                             if (sf::Keyboard::isKeyPressed(k))
                             {
-                                m_container.handle(k);
+                                m_tetrominoContainer.handle(k);
                                 break;
                             }
                     break;
@@ -51,9 +51,9 @@ void GameUI::UI::run()
                     break;
             };
         m_window.clear(sf::Color::Black);
-        m_container.render();
-        if (m_container.isGameOver)
-            m_gameOver_menu.render();
+        m_tetrominoContainer.render();
+        if (m_tetrominoContainer.isGameOver)
+            m_gameOverMenu.render();
         m_window.display();
     }
 }
--- src/GameUI/UI.hpp
+++ src/GameUI/UI.hpp
@@ -1,13 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include "../GameEntity/TetrominoContainer.hpp"
 #include "../Menu/GameOver.hpp"
+
 namespace GameUI
 {
     class UI
     {
     private:
-        GameEntity::TetrominoContainer m_container;
-        Menu::GameOver m_gameOver_menu;
+        GameEntity::TetrominoContainer m_tetrominoContainer;
+        Menu::GameOver m_gameOverMenu;
         sf::RenderWindow m_window;
         
     public:
