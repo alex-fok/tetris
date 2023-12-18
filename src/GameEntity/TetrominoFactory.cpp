cfd253ba25d6132c6355667fbd56a822e9c027a9
Alex Fok <foktmalexander@gmail.com>
2023-12-18 03:33:32 -0800

    Add bag mechaism to random generator

--- src/GameEntity/TetrominoFactory.cpp
+++ src/GameEntity/TetrominoFactory.cpp
@@ -5,20 +5,52 @@
 GameEntity::TetrominoFactory::TetrominoFactory() :
     m_currIdx(0)
 {
-    for (size_t i = 0; i < m_tetro_size; ++i)
-        m_tetros.push_back(new Tetromino(m_currIdx + i, GameEntity::BlockType(rand() % 7)));
-
-    m_currIdx += m_tetro_size;
     srand((unsigned int)time(NULL));
+    init();
+}
+
+void GameEntity::TetrominoFactory::init()
+{
+    int *first_bag = getBag();
+    while (
+        first_bag[0] == GameEntity::BlockType::O ||
+        first_bag[0] == GameEntity::BlockType::S ||
+        first_bag[0] == GameEntity::BlockType::Z
+    ) {
+        delete(first_bag);
+        first_bag = getBag();
+    }
+    addTetros(first_bag); // First
+    addTetros(getBag()); // Second
+}
+
+void GameEntity::TetrominoFactory::addTetros(int *bag)
+{
+    for (size_t i = 0; i < m_bag_size; ++i)
+        m_tetros.push_back(new Tetromino(m_currIdx++ % 256, GameEntity::BlockType(bag[i]))); 
+
+    delete(bag);
+}
+
+int *GameEntity::TetrominoFactory::getBag()
+{
+    // Create Bag
+    int *bag = new int[m_bag_size] {0, 1, 2, 3, 4, 5, 6};
+    for (size_t i = 0; i < m_bag_size - 1; ++i)
+    {
+        int j = rand() % (m_bag_size - i) + i;
+        std::swap(bag[i], bag[j]);
+    }
+    return bag;
 }
 
 GameEntity::Tetromino * GameEntity::TetrominoFactory::getNext()
 {
     Tetromino *front = m_tetros.front();
     m_tetros.pop_front();
-    m_tetros.push_back(new Tetromino(m_currIdx, GameEntity::BlockType(rand() % 7)));
+    if (m_tetros.size() < m_bag_size + 1)
+        addTetros(getBag());
     
-    m_currIdx = (m_currIdx + 1) % 256;
     return front;
 }
 
@@ -30,8 +62,5 @@ void GameEntity::TetrominoFactory::reset()
     m_tetros.clear();
     m_currIdx = 0;
 
-    for (size_t i = 0; i < m_tetro_size; ++i)
-        m_tetros.push_back(new Tetromino(m_currIdx + i, GameEntity::BlockType(rand() % 7)));
-
-    m_currIdx += m_tetro_size;
+    init();
 }
--- src/GameEntity/TetrominoFactory.hpp
+++ src/GameEntity/TetrominoFactory.hpp
@@ -8,9 +8,13 @@ namespace GameEntity
     class TetrominoFactory
     {
     private:
-        static const size_t m_tetro_size = 4;
+        static const size_t m_tetro_size = 14;
+        static const size_t m_bag_size = 7;
         std::deque<Tetromino *> m_tetros;
         unsigned short m_currIdx;
+        void init();
+        void addTetros(int *bag);
+        int *getBag();
     public:
         TetrominoFactory();
         Tetromino *getNext();
